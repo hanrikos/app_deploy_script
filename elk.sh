8c78a18031caa2a6887f8a6926c57cd47cec0422
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-06-16 14:53:10 +0300

    Update elk.sh

--- elk.sh
+++ elk.sh
@@ -5,19 +5,19 @@ echo "Grabbing IPs..."
 PRIVATE_IP=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com)
 
 echo "Installing dependencies..."
-apt-get -qq update &>/dev/null
-apt-get -yqq install unzip &>/dev/null
-apt-get install dnsmasq
+sudo apt-get -qq update &>/dev/null
+sudo apt-get -yqq install unzip &>/dev/null
+sudo apt-get install dnsmasq
 
 echo "Installing Docker..."
-apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
-apt-cache policy docker-ce
-apt-get install docker-ce -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt-get install docker-ce -y
 
-tee /etc/consul.d/elk.json > /dev/null <<EOF
+sudo tee /etc/consul.d/elk.json > /dev/null <<EOF
 {"service": {
     "name": "elk",
     "tags": ["elk"],
@@ -30,15 +30,15 @@ tee /etc/consul.d/elk.json > /dev/null <<EOF
 }
 EOF
 
-systemctl restart docker.service
+sudo systemctl restart docker.service
 
-systemctl daemon-reload
-systemctl enable consul.service
-systemctl start consul.service
+sudo systemctl daemon-reload
+sudo systemctl enable consul.service
+sudo systemctl start consul.service
 
-systemctl restart docker.service
+sudo systemctl restart docker.service
 systemctl daemon-reload
 systemctl start consul
 
-docker pull docker.elastic.co/elasticsearch/elasticsearch:6.5.4
-docker run -p 9300:9300 -p 9200:9200 -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" -e "discovery.type=single-node" --restart=always docker.elastic.co/elasticsearch/elasticsearch:6.5.4
+sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:6.5.4
+sudo docker run -p 9300:9300 -p 9200:9200 -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" -e "discovery.type=single-node" --restart=always docker.elastic.co/elasticsearch/elasticsearch:6.5.4
