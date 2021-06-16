3e7ccb610fe37b358d66d36e0044f92daa5a4734
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-06-16 14:53:37 +0300

    Update kibana.sh

--- kibana.sh
+++ kibana.sh
@@ -5,11 +5,11 @@ echo "Grabbing IPs..."
 PRIVATE_IP=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com)
 
 echo "Installing dependencies..."
-apt-get -qq update &>/dev/null
-apt-get -yqq install unzip &>/dev/null
-apt-get install dnsmasq
+sudo apt-get -qq update &>/dev/null
+sudo apt-get -yqq install unzip &>/dev/null
+sudo apt-get install dnsmasq
 
-tee /tmp/kibana.yml > /dev/null <<"EOF"
+sudo tee /tmp/kibana.yml > /dev/null <<"EOF"
 ---
 server.name: kibana
 server.host: "0.0.0.0"
@@ -17,16 +17,16 @@ elasticsearch.url: "PRIVATE_IP:9200"
 EOF
 
 echo "Installing Docker..."
-apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
-apt-cache policy docker-ce
-apt-get install docker-ce -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt-get install docker-ce -y
 
-systemctl restart docker.service
+sudo systemctl restart docker.service
 
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
-docker pull docker.elastic.co/kibana/kibana:6.5.4
-docker run -p 5601:5601 -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" -v /tmp/kibana.yml:/usr/share/kibana/config/kibana.yml --restart=always docker.elastic.co/kibana/kibana:6.5.4
+sudo docker pull docker.elastic.co/kibana/kibana:6.5.4
+sudo docker run -p 5601:5601 -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" -v /tmp/kibana.yml:/usr/share/kibana/config/kibana.yml --restart=always docker.elastic.co/kibana/kibana:6.5.4
