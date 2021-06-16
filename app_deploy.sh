f32f85f3956d10faf039db91ca2340ffa15f3c12
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-06-16 14:52:32 +0300

    Update app_deploy.sh

--- app_deploy.sh
+++ app_deploy.sh
@@ -5,11 +5,11 @@ echo "Grabbing IPs..."
 PRIVATE_IP=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com)
 
 echo "Installing dependencies..."
-apt-get -qq update &>/dev/null
-apt-get -yqq install unzip &>/dev/null
-apt-get install dnsmasq
+sudo apt-get -qq update &>/dev/null
+sudo apt-get -yqq install unzip &>/dev/null
+sudo apt-get install dnsmasq
 
-tee /tmp/filebeat.yml > /dev/null <<EOF
+sudo tee /tmp/filebeat.yml > /dev/null <<EOF
 filebeat.prospectors:
 - type: log
   enabled: true
@@ -25,33 +25,33 @@ setup.template.pattern: "$PRIVATE_IP-*-Pattern"
 EOF
 
 echo "Installing Docker..."
-apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
-apt-cache policy docker-ce
-apt-get install docker-ce -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt-get install docker-ce -y
 
 
-systemctl restart docker.service
+sudo systemctl restart docker.service
 
-systemctl daemon-reload
-systemctl enable consul.service
-systemctl start consul.service
+sudo systemctl daemon-reload
+sudo systemctl enable consul.service
+sudo systemctl start consul.service
 
-systemctl restart docker.service
-docker pull hansmuller/midproject:latest
-docker pull docker.elastic.co/beats/filebeat:6.5.4
-docker run -p 65433:65433 -v /tmp:/tmp --restart always hansmuller/midproject:latest &
+sudo systemctl restart docker.service
+sudo docker pull hansmuller/midproject:latest
+sudo docker pull docker.elastic.co/beats/filebeat:6.5.4
+sudo docker run -p 65433:65433 -v /tmp:/tmp --restart always hansmuller/midproject:latest &
 
-docker run -v /tmp/filebeat.yml:/usr/share/filebeat/filebeat.yml -v /tmp:/tmp:ro --restart=always docker.elastic.co/beats/filebeat:6.5.4 &
+sudo docker run -v /tmp/filebeat.yml:/usr/share/filebeat/filebeat.yml -v /tmp:/tmp:ro --restart=always docker.elastic.co/beats/filebeat:6.5.4 &
 
-tee /tmp/logmaker.sh > /dev/null <<EOF
+sudo tee /tmp/logmaker.sh > /dev/null <<EOF
 while :
 do
     curl "localhost:65433" --output "/tmp/app_log.txt"
     sleep 5s
 done
 EOF
-chmod +x /tmp/logmaker.sh
+sudo chmod +x /tmp/logmaker.sh
 exec /tmp/logmaker.sh&
