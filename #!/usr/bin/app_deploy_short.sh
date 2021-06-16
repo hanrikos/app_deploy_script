fd974be133eb942eb4ed7cfe46b4583d0dfd36a8
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-06-16 12:03:47 +0300

    Delete app_deploy_short.sh

--- #!/usr/bin/app_deploy_short.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-git clone hhttps://github.com/hanrikos/app_deploy_script.git /tmp/deploy
-
-sudo chmod +x /tmp/deploy/app_deploy_script/app_deploy.sh
-exec /tmp/deploy/app_deploy_script/app_deploy.sh&
