88b0c0708d39e481c86483027be984aebd992c98
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-06-16 10:38:45 +0300

    Create elk.sh

---
+++ elk.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Grabbing IPs..."
+PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+echo "Installing dependencies..."
+sudo apt-get -qq update &>/dev/null
+sudo apt-get -yqq install unzip &>/dev/null
+sudo apt-get install dnsmasq
+
+echo "Installing Docker..."
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt-get install docker-ce -y
+
+sudo systemctl restart docker.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable consul.service
+sudo systemctl start consul.service
+
+sudo systemctl restart docker.service
+systemctl daemon-reload
+systemctl start consul
+
+sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:6.5.4
+sudo docker run -p 9300:9300 -p 9200:9200 -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" -e "discovery.type=single-node" --restart=always docker.elastic.co/elasticsearch/elasticsearch:6.5.4
