6c2f083c19421de1636cba500118d774d3c2858c
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-06-26 22:08:15 +0300

    Update app_deploy.sh

--- app_deploy.sh
+++ app_deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Grabbing IPs..."
-PRIVATE_IP=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com)
+PRIVATE_IP="192.168.65.88"
 
 echo "Installing dependencies..."
 sudo apt-get -qq update &>/dev/null
