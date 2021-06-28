62ae094b4652be9174c848c62a36a12547511950
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-06-28 13:15:38 +0300

    Update prometheus_deploy.sh

--- prometheus_deploy.sh
+++ prometheus_deploy.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
-set -e
 
-echo "Installing dependencies..."
-sudo apt-get -qq update &>/dev/null
-sudo apt-get -yqq install unzip &>/dev/null
+version="${VERSION:-1.0.1}"
+arch="${ARCH:-linux-amd64}"
+bin_dir="${BIN_DIR:-/usr/local/bin}"
 
-echo "Installing Docker..."
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt-cache policy docker-ce
-sudo apt-get install docker-ce -y
+wget "https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.$arch.tar.gz" \
+    -O /tmp/node_exporter.tar.gz
 
-sudo systemctl restart docker.service
+mkdir -p /tmp/node_exporter
 
-sudo tee /tmp/prometheus.yml > /dev/null <<"EOF"
-global:
-  scrape_interval:     10s
-  evaluation_interval: 10s
-rule_files:
-  # - "first.rules"
-  # - "second.rules"
-scrape_configs:
-  - job_name: prometheus
-    static_configs:
-      - targets: ['localhost:9090']
+cd /tmp || { echo "ERROR! No /tmp found.."; exit 1; }
+
+tar xfz /tmp/node_exporter.tar.gz -C /tmp/node_exporter || { echo "ERROR! Extracting the node_exporter tar"; exit 1; }
+
+cp "/tmp/node_exporter/node_exporter-$version.$arch/node_exporter" "$bin_dir"
+chown root:staff "$bin_dir/node_exporter"
+
+cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Prometheus node exporter
+After=local-fs.target network-online.target network.target
+Wants=local-fs.target network-online.target network.target
+[Service]
+Type=simple
+ExecStartPre=-/sbin/iptables -I INPUT 1 -p tcp --dport 9100 -s 127.0.0.1 -j ACCEPT
+ExecStartPre=-/sbin/iptables -I INPUT 3 -p tcp --dport 9100 -j DROP
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
 EOF
 
-sudo systemctl daemon-reload
+systemctl enable node_exporter.service
+systemctl start node_exporter.service
 
-sudo systemctl restart docker.service
-sudo docker pull prom/prometheus
-sudo docker run -p 9090:9090 -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml --restart=always prom/prometheus
+echo "SUCCESS! Installation succeeded!"
