3ecd49dec67cfb21d1927847485754bf7602bf9e
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-07-01 16:16:18 +0300

    Update elk_all_in_one_short.sh

--- elk_all_in_one_short.sh
+++ elk_all_in_one_short.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo yum install git -y
-sudo git clone hhttps://github.com/hanrikos/app_deploy_script.git /tmp/deploy
+sudo git clone https://github.com/hanrikos/app_deploy_script.git /tmp/deploy
 
 sudo chmod +x /tmp/deploy/app_deploy_script/elk_all_in_one.sh
 exec /tmp/deploy/app_deploy_script/elk_all_in_one.sh &
