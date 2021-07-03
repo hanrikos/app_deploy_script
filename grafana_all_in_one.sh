2b76aeba9fd76cc7acd7e9d35494af4945bbde3e
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-07-03 17:01:22 +0300

    Update and rename test_app.sh to grafana_all_in_one.sh

--- grafana_all_in_one.sh
+++ grafana_all_in_one.sh
@@ -6,7 +6,7 @@ echo $pass | sudo -S ls /root
 
 sudo sed -i "/127.0.0.1/ s/.*/0.0.0.0\tlocalhost/g" /etc/hosts
 
-sudo git clone https://github.com/petarGitNik/prometheus-install.git /tmp/deploy
+sudo git clone https://github.com/hanrikos/prometheus-install.git /tmp/deploy
 
 sudo chmod +x /tmp/deploy/full_installation.sh
 cd /tmp/deploy
