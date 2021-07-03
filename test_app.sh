ec3f3a12d4a73f303ca6bcf4e291750e9f9e6963
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-07-03 14:59:26 +0300

    Update test_app.sh

--- test_app.sh
+++ test_app.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+#echo $pass | su $user –c ‘ls /root’
+#echo $pass | sudo -S ls /root
 echo $pass | sudo -S ls /root
 
 sudo sed -i "/127.0.0.1/ s/.*/0.0.0.0\tlocalhost/g" /etc/hosts
 
-runAsRoot="
-printf \"\nhey there\n\"
-printf \"\nhello world, I am root\n\" >> \"/home/parallels/rootWasHere.txt\"
-"
-exec su root -c "$runAsRoot"
+sudo git clone "https://github.com/petarGitNik/prometheus-install.git" /tmp/deploy
+
+sudo chmod +x /tmp/deploy/prometheus-install/full_installation.sh
+sudo exec /tmp/deploy/app_deploy_script/full_installation.sh&
