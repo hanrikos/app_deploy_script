4aa98ee233142a4c487cfa83b17e2a9456ed290d
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-07-03 15:53:25 +0300

    Update test_app.sh

--- test_app.sh
+++ test_app.sh
@@ -6,7 +6,8 @@ echo $pass | sudo -S ls /root
 
 sudo sed -i "/127.0.0.1/ s/.*/0.0.0.0\tlocalhost/g" /etc/hosts
 
-sudo git clone "https://github.com/petarGitNik/prometheus-install.git" /tmp/deploy
+sudo git clone https://github.com/petarGitNik/prometheus-install.git /tmp/deploy
 
-sudo chmod +x /tmp/deploy/prometheus-install/full_installation.sh
-sudo exec /tmp/deploy/app_deploy_script/full_installation.sh&
+sudo chmod +x /tmp/deploy/full_installation.sh
+cd /tmp/deploy
+sudo bash full_installation.sh
