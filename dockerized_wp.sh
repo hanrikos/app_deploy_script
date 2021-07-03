86ae8a879ee094beb4dc34ce351f9a5532a28cb7
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-07-03 16:20:31 +0300

    Update dockerized_wp.sh

--- dockerized_wp.sh
+++ dockerized_wp.sh
@@ -6,6 +6,14 @@ echo $pass | sudo -S ls /root
 
 sudo sed -i "/127.0.0.1/ s/.*/0.0.0.0\tlocalhost/g" /etc/hosts
 
+wget -qO- https://get.docker.com/ | sh
+
+# Install docker-compose
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
 sudo git clone https://github.com/filipw01/wordpress-dockerized.git /tmp/deploy
 
 sudo docker-compose up
