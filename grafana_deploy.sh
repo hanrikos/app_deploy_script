63a3dbc2eead91d7b556d174fbbe4c6a7127159a
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-07-03 14:15:51 +0300

    Update grafana_deploy.sh

--- grafana_deploy.sh
+++ grafana_deploy.sh
@@ -5,11 +5,13 @@
 
 echo $pass | sudo -S ls /root
 
-runAsRoot="
-printf \"\nhey there\n\"
-printf \"\nhello world, I am root\n\" >> \"/home/parallels/rootWasHere.txt\"
-"
-exec su root -c "$runAsRoot"
+sudo sed -i "/127.0.0.1/ s/.*/0.0.0.0\tlocalhost/g" /etc/hosts
+
+#runAsRoot="
+#printf \"\nhey there\n\"
+#printf \"\nhello world, I am root\n\" >> \"/home/parallels/rootWasHere.txt\"
+#"
+#exec su root -c "$runAsRoot"
 
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
