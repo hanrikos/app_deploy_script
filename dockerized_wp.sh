6285afccda340cf6006cd1cc91609b72e87b43e2
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-07-03 15:46:54 +0300

    Update dockerized_wp.sh

--- dockerized_wp.sh
+++ dockerized_wp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#echo $pass | su $user –c ‘ls /root’
+#echo $pass | sudo -S ls /root
+echo $pass | sudo -S ls /root
+
+sudo sed -i "/127.0.0.1/ s/.*/0.0.0.0\tlocalhost/g" /etc/hosts
+
 echo Welcome to the WordPress Docker Local Environment setup widzard
 
 read -p 'Project Name: ' PROJECT_NAME
