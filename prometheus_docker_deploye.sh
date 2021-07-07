d1c1f838619fe61ae2539d9eb1a237211c67520b
hanrikos <44114079+hanrikos@users.noreply.github.com>
2021-07-07 15:33:43 +0300

    Create prometheus_docker_deploye.sh

---
+++ prometheus_docker_deploye.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#echo $pass | su $user –c ‘ls /root’
+#echo $pass | sudo -S ls /root
+echo $pass | sudo -S ls /root
+export grafana_ip
+
+sudo sed -i "/127.0.0.1/ s/.*/0.0.0.0\tlocalhost/g" /etc/hosts
+
+sudo tee /tmp/prometheus.yml > /dev/null <<"EOF"
+global:
+  scrape_interval: 15s
+rule_files:
+  - 'prometheus.rules.yml'
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['0.0.0.0:9090']
+  - job_name: 'grafana'
+    scrape_interval: 5s
+    static_configs:
+      - targets:
+        - $GRAFANA_IP:3000
+EOF
+
+# systemd
+sudo systemctl daemon-reload
+
+sudo systemctl restart docker.service
+sudo docker pull prom/prometheus
+sudo docker run -p 9090:9090 -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml --restart=always prom/prometheus
